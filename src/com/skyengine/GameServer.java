1689d78fdd890dff24fbbfa74945dfdf30563ee3
Insenthium <Insenthium@hotmail.com>
2020-12-24 17:35:58 -0500

    Initial GameServer Configurations
    added logger to the GameServer class
    Custom format for the console stream

--- src/com/skyengine/GameServer.java
+++ src/com/skyengine/GameServer.java
@@ -1,5 +1,8 @@
 package com.skyengine;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import com.skyengine.util.ConsoleStream;
 
 /**
@@ -12,14 +15,30 @@ public class GameServer
 {
 
     /**
+     * Logger for the {@docRoot GameServer} class
+     */
+    private static final Logger logger = Logger.getLogger(GameServer.class.getName());
+    
+    public GameServer() 
+    {
+	//Sets the logger level to the appropriate setting for debugging purposes
+	logger.setLevel(Level.SEVERE);
+    }
+    
+    /**
      * Application entry-point.
      * @param args program parameters
      */
     public static void main(String[] args)
     {
+	logger.info("Initializing Sky-Engine...");
+	
 	//Sets a custom console stream format
 	System.setOut(new ConsoleStream(System.out));
 
+	logger.info("Setting console stream format...");
+	
+	
     }
 
 
